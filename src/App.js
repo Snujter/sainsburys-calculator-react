984b39e577a51ed9b03b5c5b8a07f8cb20c08f1e
Tamas Schneider <snaaajder@gmail.com>
2019-10-30 19:39:49 +0000

    Fix missing import statement

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import './App.css';
 import OrderTable from "./components/OrderTable";
+import { CSVLink } from "react-csv";
 
 const setupPayments = (rawItems, payers) => {
     let payments = [];
