fd6ffb17209a36b6903452b2c1853912c6b902bd
Tamas Schneider <snaaajder@gmail.com>
2020-01-21 15:54:34 +0000

    Add forgotten method for formatting date

--- src/models/Delivery.js
+++ src/models/Delivery.js
@@ -11,4 +11,21 @@ export const DeliveryModel = types
         get pricePerPerson() {
             return Math.floor(self.price / self.payers.length);
         },
+        get formattedDate() {
+            const year = self.date.getFullYear();
+            let month = '' + (self.date.getMonth() + 1),
+                day = '' + self.date.getDate();
+
+            if (month.length < 2) {
+                month = '0' + month;
+            }
+            if (day.length < 2) {
+                day = '0' + day;
+            }
+
+            console.log([year, month, day]);
+            console.log(self.date);
+
+            return [year, month, day].join('-');
+        }
     }));
